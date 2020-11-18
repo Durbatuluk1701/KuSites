bb8c76121f3d46dda4d38160f9b09d1952153813
Thomas, Will P <30wthomas@ku.edu>
2020-11-18 12:22:02 -0600

    Admin page and CreateUser creation

---
+++ 448-Projects/lab10/CreateUser/CreateUser.php
@@ -0,0 +1,34 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "willthomas", "ANahy9ui", "willthomas");
+
+$userId = $_POST["userId"];
+
+if ($mysqli->connect_error) {
+    die("Connect Failed: ". $mysqli->connect_error);
+}
+
+$checkQuery = "select * from Users where user_id = \"$userId\";";
+
+$insertQuery = "INSERT INTO Users (user_id) values (\"$userId\");";
+
+// echo $insertQuery;
+// echo $checkQuery;
+
+$result = $mysqli->query($checkQuery);
+// echo $result2;
+echo "<h1>Database Response</h1>";
+if ($result->fetch_row()[0]) {
+    // Already in DB
+    echo "<h3>User '". $userId . "' Already in the Database, cannot duplicate users</h3>";
+    
+} else {
+
+    $mysqli->query($insertQuery);
+    echo "<h3>Successfull Inserted '" . $userId . "' into Database</h3>";
+}
+
+?>
